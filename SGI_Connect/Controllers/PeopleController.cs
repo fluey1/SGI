89dddc13ae66279f75e5d05c31c4475a981cd2b7
Alain Moussa <amoussa@septimatech.com>
2019-11-17 23:03:02 -0500

    update

--- SGI_Connect/Controllers/PeopleController.cs
+++ SGI_Connect/Controllers/PeopleController.cs
@@ -9,21 +9,43 @@ namespace SGI_Connect.Controllers
 {
     public class PeopleController : Controller
     {
+        List<PersonModel> people = new List<PersonModel>();
+        
         // GET: People
         public ActionResult Index()
         {
-            
+            people.Add(new PersonModel { FirstName = "Alain", LastName = "Moussa", Age = 38 });
+            people.Add(new PersonModel { FirstName = "Joe", LastName = "Smith", Age = 56 });
+            people.Add(new PersonModel { FirstName = "Sarah", LastName = "Connor", Age = 25 });
             return View();
         }
 
         public ActionResult ListPeople()
         {
-            List<PersonModel> people = new List<PersonModel>();
-            people.Add(new PersonModel { FirstName = "Alain", LastName = "Moussa", Age = 38});
-            people.Add(new PersonModel { FirstName = "Joe", LastName = "Smith", Age = 56 });
-            people.Add(new PersonModel { FirstName = "Sarah", LastName = "Connor", Age = 25 });
+            
+            
 
             return View(people);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(PersonModel person)
+        {
+            if(ModelState.IsValid)
+            {
+                people.Add(person);
+                return RedirectToAction("ListPeople");
+            }
+            else
+            {
+                return View(person);
+            }
+            
         }
     }
 }
