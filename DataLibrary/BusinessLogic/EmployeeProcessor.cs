396e4e81f3ccb969ef1e176efa6a2b21d74f9318
Alain Moussa <amoussa@septimatech.com>
2019-11-18 01:12:10 -0500

    added a demo project

---
+++ DataLibrary/BusinessLogic/EmployeeProcessor.cs
@@ -0,0 +1,42 @@
+﻿using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using DataLibrary.DataAccess;
+
+namespace DataLibrary.BusinessLogic
+{
+    public static class EmployeeProcessor
+    {
+        public static int CreateEmployee(int employeeId,string firstname,
+            string lastname, string emailAddress) 
+        {
+            EmployeeModel data = new EmployeeModel
+            {
+                EmployeeId = employeeId,
+                FirstName = firstname,
+                LastName = lastname,
+                EmailAddress = emailAddress
+            };
+
+            string sql = @"insert into dbo.Employee (EmployeeId, FirstName, LastName, EmailAddress)
+                            values (@EmployeeId, @FirstName, @LastName, @EmailAddress);";
+            
+            return SqlDataAccess.SaveData(sql, data);
+        }
+
+        public static List<EmployeeModel> LoadEmployees()
+        {
+            string sql = @"select EmployeeId, FirstName, LastName, EmailAddress
+                            from dbo.Employee;";
+
+            return SqlDataAccess.LoadData<EmployeeModel>(sql);
+        }
+    }
+}
---
+++ MVCDemoDB/dbo/Tables/Employee.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [EmployeeId] INT NOT NULL, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [EmailAddress] NVARCHAR(100) NULL 
+)
